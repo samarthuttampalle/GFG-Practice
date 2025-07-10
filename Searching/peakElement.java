54a96e7c57a34f8db6dd8ac1bc916d945edcf62e
Samarth Uttampalle <suttampalle@gmail.com>
2025-07-10 19:27:27 +0530

    return the index of the peak element

---
+++ Searching/peakElement.java
@@ -0,0 +1,23 @@
+public class peakElement {
+    public int peakElement(int[] arr) {
+        // code here
+        int n = arr.length; 
+        int low = 0;
+        int high = n-1;
+        while(low<=high){
+            int mid = (low+high)/2;
+            
+            if((mid==0||arr[mid-1]<=arr[mid]) && (mid==n-1||arr[mid+1]<=arr[mid])){
+                return mid;
+            }
+            if(mid>0 && arr[mid-1]>arr[mid]){
+                high = mid-1;
+            }
+            else{
+                low = mid+1;
+            }
+        }
+        return -1;
+    }
+    
+}
