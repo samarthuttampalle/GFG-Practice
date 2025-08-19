fec33d170bce4f5d45bccccf3e47ad1d960e2803
Samarth Uttampalle <suttampalle@gmail.com>
2025-08-19 19:02:21 +0530

    Sort 0's 1's 2's

---
+++ Sorting/Sorting.java
@@ -0,0 +1,27 @@
+public class Sorting {
+    public static void segragate012(int arr[], int n) {
+        // Your Code Here
+        int low=0,high=n-1,mid=0;
+        
+        while(mid<=high){
+            if(arr[mid]==0){
+                swap(arr,low,mid);
+                low++;
+                mid++;
+            }
+            else if(arr[mid]==1){
+                mid++;
+            }
+            else{
+                swap(arr,mid,high);
+                high--;
+            }
+            
+        }
+    }
+    public static void swap(int[] arr,int i,int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
